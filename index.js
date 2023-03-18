b32c095c431c5fa5f516b7689f2611f7d3301efa
Lewis <gluwiz@gmail.com>
2023-03-18 21:07:39 +0300

    javascript-functions-continued lab attempt?

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function razzle() {
+    console.log("You've been razzled!");
+  }
+
+  function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  console.log(saturdayFun()); // This Saturday, I want to roller-skate!
+console.log(saturdayFun("paint the town red")); // This Saturday, I want to paint the town red!
